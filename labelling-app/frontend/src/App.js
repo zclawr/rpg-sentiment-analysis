d5ad763b6df6055a25e5c2a766463106b96cd9eb
ShawnPana <spana@ucsd.edu>
2024-10-27 19:22:02 -0700

    init

---
+++ labelling-app/frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div className="App">
+    <h1 style={{ textAlign: 'center' }}>Movie Line Sentiment Rating</h1>
+    <Home />
+  </div>
+  );
+}
+
+export default App;
